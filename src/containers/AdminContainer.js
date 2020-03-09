2d1d27e7e86098b4ea238a3d4751ffe344d5c2c1
Bri Fetterley <bnfetterley@usfca.edu>
2020-03-09 12:45:12 -0400

    Create Admin Modal working, Admin Show implemented, next step: Applicants Container/Show

--- src/containers/AdminContainer.js
+++ src/containers/AdminContainer.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
+import CreateAdminModal from '../components/CreateAdminModal'
+import Admin from '../components/Admin'
 
 export default class AdminContainer extends Component {
     render() {
         return (
             <div>
            
+         {this.props.admins.map(admin => <li>  {admin} </li>)
+         
+         }
 
-         <button onClick={this.props.displayFormClick}>Create an Admin</button>       
+
+
+         <button onClick={this.props.displayFormClick}>Create an Admin</button>   
+         <CreateAdminModal
+         displayFormClick = {this.props. displayFormClick}
+        displayModal = {this.props.displayModal}
+        />    
             </div>
         )
     }
